4d425e20db2882311413ed7929a9206767135301
Ali Adldoust <aliadldoust1998@gmail.com>
2026-08-24 13:21:59 +0330

    adding files

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+let resultEl = document.getElementById("result-el")
+
+let Value = 0
+
+
+function plus1(){
+
+    Value += 1
+    resultEl.textContent = Value
+
+}
+
+
+
+function plus2(){
+
+    Value += 2
+    resultEl.textContent = Value
+    
+}
+
+
+
+
+function plus3(){
+
+    Value += 3
+    resultEl.textContent = Value
+    
+}
